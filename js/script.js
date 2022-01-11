9ab9cd6c6a31a23fe07078e787683284b2d484b6
ebfaler <74653573+ebfaler@users.noreply.github.com>
2022-01-11 16:37:27 +0100

    added pagination

--- js/script.js
+++ js/script.js
@@ -15,19 +15,19 @@ const itemsPerPage = 9;
 */
 function showPage(list, page) {
 
-// list and page parameters represent student data that will be passed as an argument when the function is called.
+   // list and page parameters represent student data that will be passed as an argument when the function is called.
 
-let startIndex = (page* itemsPerPage) - itemsPerPage;
-let endIndex = page* itemsPerPage;
-let studentList = document.querySelector('.student-list');
+   let startIndex = (page * itemsPerPage) - itemsPerPage;
+   let endIndex = (page * itemsPerPage) - 1;
+   let studentList = document.querySelector('.student-list');
 
-// setting student-list variable to an empty string will remove any students that might have previously been displayed.
-studentList.innerHTML = "";
+   // setting student-list variable to an empty string will remove any students that might have previously been displayed.
+   studentList.innerHTML = "";
 
-for(let i=0; i<list.length; i++) {
- if( i >= startIndex && i <= endIndex) {
+   for (let i = 0; i < list.length; i++) {
+      if (i >= startIndex && i <= endIndex) {
 
-   let studentItem = `<li class="student-item cf">
+         let studentItem = `<li class="student-item cf">
     <div class="student-details">
       <img class="avatar" src="https://randomuser.me/api/portraits/women/25.jpg" alt="Profile Picture">
       <h3>Ethel Dean</h3>
@@ -38,20 +38,61 @@ for(let i=0; i<list.length; i++) {
     </div>
   </li>`;
 
-  studentList.innerHTML= studentItem;
- }
-}
-
+         studentList.insertAdjacentHTML('beforeend', studentItem);
+      }
+   }
 
 };
-
+//calling the function
 showPage(data, 1);
+
+
 
 /*
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
 
+function addPagination(list) {
+
+   let paginationButtons = Math.ceil((list.length) / itemsPerPage);
+   // console.log(paginationButtons);
+   let linkList = document.querySelector('.link-list');
+   linkList.innerHTML = "";
+
+   for (let i = 0; i < paginationButtons; i++) {
+
+      let button = (`<li>
+                    <button type="button">${i + 1}</button>
+                    </li>`);
+
+      linkList.insertAdjacentHTML('beforeend', button);
+   }
 
 
-// Call functions
+   const buttons = linkList.getElementsByTagName("button");
+   buttons[0].className = "active";
+
+   /* click event listener for linkList*/
+
+   linkList.addEventListener('click', (e) => {
+
+      if (e.target.tagName === "BUTTON") {
+         // loop over any button that has class active and remove class
+         for (let i = 0; i < buttons.length; i++) {
+            if (buttons[i].classList.contains("active")) {
+               buttons[i].classList.remove("active");
+            }
+         }
+         // adding active class to button clicked and passing textContent to load the new page
+         e.target.className = "active";
+         let newPage = e.target.textContent;
+         showPage(list, newPage);
+      }
+   })
+};
+//calling the function
+addPagination(data);
+
+
+
